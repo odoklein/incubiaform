5addb94df01ee8fb24c7129bdc4e46f05c3e839c
Salma HADRI-KHOUSSA <salmahadri.k@gmail.com>
2025-03-24 14:54:41 +0100

    Update settings.py

--- incubiaform/settings.py
+++ incubiaform/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!--mq3c+k@_(ssu9mq!owz3d(#pim)korp!kj@r70g#__rxv6!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['incubiaform-production.up.railway.app']
 
 
 # Application definition
