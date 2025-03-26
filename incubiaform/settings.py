326f29a297ddf22975c37d62d46a426fc51b90d0
Slimani Mohammed <150338098+odoklein@users.noreply.github.com>
2025-03-26 09:15:11 +0100

    Update settings.py

--- incubiaform/settings.py
+++ incubiaform/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!--mq3c+k@_(ssu9mq!owz3d(#pim)korp!kj@r70g#__rxv6!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['incubiaform.onrender.com/']
+ALLOWED_HOSTS = ['https://incubiaform-oi31.onrender.com']
 
 print(f"ALLOWED_HOSTS: {ALLOWED_HOSTS}")
 # Application definition
